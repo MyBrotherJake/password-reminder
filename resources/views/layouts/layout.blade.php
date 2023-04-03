06727be8b3158aacceccb7a0370e5fcac5dcc128
jake <jake.is.mybrother@gmail.com>
2023-04-03 14:50:31 +0900

    add: CSS

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">  
+  <title>
+    @yield('title')
+  </title>
+</head>
+<body>
+  <h1>
+    @yield('title')
+  </h1>
+  @section('menubar')
+  <h2 class="menutitle">Menu</h2>
+  <ul>
+    <li>@show</li>
+  </ul>
+  <hr size="1">
+  <div class="content">
+    @yield('content')
+  </div>    
+  <div class="footer">
+    @yield('footer')
+  </div>  
+</body>
+</html>
